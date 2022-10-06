4593a13260be58f1cf33d289753972f9ece26550
Xatai123 <xataialiyev5@gmail.com>
2022-10-06 22:19:11 +0400

    moved lobby components to home page

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,17 +1,9 @@
-import LobbyCard from '../components/LobbyCard/LobbyCard';
-import LobbyDetails from '../components/LobbyDetails/LobbyDetails';
-import Lobbies from '../fakeDB/Lobbies.js'
+import Home from "./Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        {Lobbies.map(lobby => (
-          <LobbyCard lobbyName={lobby.lobbyName} lobbyActivePlayerCount={lobby.lobbyActivePlayerCount} lobbySize={lobby.lobbySize} />
-        ))}
-      </div>
-
-      <LobbyDetails lobbyName="419boys" lobbyActivePlayerCount={419} lobbySize={45} />
+      <Home />
     </div>
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,21 @@
 import './style.scss'
-function Home({ text, color }) {
-    return (
-        <div className={'textaqua'}>
-            <p>{text}</p>
-        </div>
-    );
+import Lobbies from '../../fakeDB/Lobbies.js'
+import LobbyCard from '../../components/LobbyCard/LobbyCard'
+import LobbyDetails from '../../components/LobbyDetails/LobbyDetails';
+
+function Home() {
+
+  return (
+    <div className='Home'>
+      <div>
+        {Lobbies.map(lobby => (
+          <LobbyCard lobbyName={lobby.lobbyName} lobbyActivePlayerCount={lobby.lobbyActivePlayerCount} lobbySize={lobby.lobbySize} />
+        ))}
+      </div>
+
+      <LobbyDetails lobbyName="419boys" lobbyActivePlayerCount={419} lobbySize={45} />
+    </div>
+  );
 }
 
 export default Home;
