72d8c2ceec41ec6d53fde621832091ad6fb2498f
Turqay <turgayau@code.edu.az>
2023-01-29 14:54:09 +0400

    2nd day

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,26 @@
 import React from "react"
 
-const blog = () => {
-  return <div>nice</div>
+const blog = ({ blog }) => {
+  const limitBlogText = (text) => {
+    if (text.length >= 200) {
+      text = text.slice(0, 200) + "..."
+    }
+    return text
+  }
+  return (
+    <div className="bg-slate-50 p-4 rounded-lg ">
+      <div className="flex space-x-5 items-center pb-4">
+        <div className="w-16 h-16 overflow-hidden rounded-full">
+          <img src={blog.profilePhoto} alt="Peter" />
+        </div>
+        <div>
+          <div className="font-bold text-lg pb-1">{blog.fullName}</div>
+          <div className="text-slate-500 text-sm">{blog.occupation}</div>
+        </div>
+      </div>
+      <div className="">{limitBlogText(blog.blogText)}</div>
+    </div>
+  )
 }
 
 export default blog
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,15 +3,28 @@ import Blog from "components/Blog"
 import "./style.scss"
 
 function Home() {
+  let blog = {
+    fullName: "Peter Griffin",
+    occupation: "Software Engineer intern",
+    profilePhoto:
+      "https://i.dailymail.co.uk/1s/2021/10/04/00/48720441-10055587-image-m-34_1633302210416.jpg",
+    blogText:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, fugiat officiis velit totam fugit modi incidunt sit natus voluptates quos deserunt labore ex veniam ad a tenetur cupiditate perferendis illo?",
+  }
+  const blogs = [blog, blog, blog]
   return (
     <div className="Home">
-      <NavigationBar></NavigationBar>
+      <NavigationBar />
       <div className="container mx-auto">
         <div className="flex justify-between py-20">
           <div className="text-2xl">Recent Blogs</div>
           <button className="btn btn-blue">Create</button>
         </div>
-        <Blog></Blog>
+        <div className="grid grid-cols-2 gap-5">
+          {blogs.map((blog, i) => (
+            <Blog blog={blog} key={i} />
+          ))}
+        </div>
       </div>
     </div>
   )
