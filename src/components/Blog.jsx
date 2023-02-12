6c241ad301881dc5c5ff1b9ed9a7ade562e4948c
Turqay <turgayau@code.edu.az>
2023-02-12 19:59:29 +0400

    up

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,23 @@
-import React from "react"
+import { useState } from "react"
 
-const blog = ({ blog }) => {
+const Blog = ({ blog }) => {
   const limitBlogText = (text) => {
     if (text.length >= 200) {
       text = text.slice(0, 200) + "..."
     }
     return text
   }
+  const [text, setText] = useState(limitBlogText(blog.blogText))
+  const [isOpen, setIsOpen] = useState(false)
+  const readMore = () => {
+    setText(blog.blogText)
+    setIsOpen(true)
+  }
+  const hide = () => {
+    setText(limitBlogText)
+    setIsOpen(false)
+  }
+
   return (
     <div className="bg-slate-50 p-4 rounded-lg ">
       <div className="flex space-x-5 items-center pb-4">
@@ -18,9 +29,23 @@ const blog = ({ blog }) => {
           <div className="text-slate-500 text-sm">{blog.occupation}</div>
         </div>
       </div>
-      <div className="">{limitBlogText(blog.blogText)}</div>
+      <div className="">
+        {text}
+        {isOpen ? (
+          <span className="text-cyan-500 ml-2 cursor-pointer" onClick={hide}>
+            Hide
+          </span>
+        ) : (
+          <span
+            className="text-cyan-500 ml-2 cursor-pointer"
+            onClick={readMore}
+          >
+            Read more
+          </span>
+        )}
+      </div>
     </div>
   )
 }
 
-export default blog
+export default Blog
